0cafaad41f76b48dfe4f5d8009edc164bb19eb8e
linhsol26 <nhglinh.sol@gmail.com>
2020-02-08 15:49:34 +0700

    add user icon

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { LoginComponent } from 'src/app/dialogs/login/login.component';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,15 +11,27 @@ import { LoginComponent } from 'src/app/dialogs/login/login.component';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor(public dialog: MatDialog) { }
-
+  constructor(public dialog: MatDialog, public snackBar: MatSnackBar, public afAuth: AngularFireAuth) { }
+  avt = '';
   ngOnInit() {
-  }
+    this.afAuth.user.subscribe((usr) => {
+      if (!usr.emailVerified) {
+        usr.sendEmailVerification().then(() => {
+          this.afAuth.auth.signOut();
+          this.snackBar.open('Please check your email to verify your account', 'OK', {duration: 5000});
+        }).catch((err) => {
+          this.snackBar.open(err, 'OK', {duration: 5000});
+        });
 
-  loginOnClick() {
+      }
+      this.avt = usr.photoURL;
+
+    });
+}
+loginOnClick() {
     this.dialog.open(LoginComponent, {
       width: '30%',
       height: '50%'
     });
-  }
+}
 }
